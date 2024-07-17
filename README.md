ce25c4b53bf34b8f49d652781810e3e7a6929648
Zach Fey <zachary.fey@risk3sixty.com>
2024-07-17 14:48:05 -0400

    Init action

---
+++ README.md
@@ -0,0 +1,30 @@
+# Update Scheduler Task Definitions with NodeJS
+
+GitHub Action that updates all schedules within a Schedule Group with the provided task definition ARN
+
+## Inputs
+
+## `region`
+
+**Required** Region the schedule-group is in.
+
+## `group_name`
+
+**Required** The name of the Schedule Group to update.
+
+## `new_task_definition_arn`
+
+**Required** The task definition to update the schedules
+
+
+## Outputs
+
+
+## Example usage
+
+```
+uses: ris3sixty/update-scheduler-task-definition-revisions@v1.0
+with:
+	region: 'us-east-1'
+	group_name: 'Schedule-Group-Name'
+	new_task_definition_arn: 'arn:aws:ecs:us-east-1:XXXX:task-definition/qa-web:2'
---
+++ index.js
@@ -0,0 +1,57 @@
+const core = require("@actions/core");
+const {
+  SchedulerClient,
+  ListSchedulesCommand,
+  GetScheduleCommand,
+  UpdateScheduleCommand,
+  //   ListTasksCommand,
+  //   ExecuteCommandCommand,
+} = require("@aws-sdk/client-scheduler");
+
+(async () => {
+  try {
+    // inputs defined in action.yml
+    const region = core.getInput('region')
+    const groupName = core.getInput('group_name')
+    const newTaskDefinitionArn = core.getInput('new_task_definition_arn')
+    const scheduler = new SchedulerClient({ region });
+
+    console.log(`Getting schedules in the group ${groupName}.`);
+    const { Schedules: schedules } = await scheduler.send(
+      new ListSchedulesCommand({ GroupName: groupName })
+    );
+
+    await Promise.all(
+      schedules.map(async (schedule) => {
+        const scheduleDetails = await scheduler.send(
+          new GetScheduleCommand(schedule)
+        );
+
+        delete scheduleDetails["$metadata"];
+
+        const updatedSchedule = {
+          ...scheduleDetails,
+          Target: {
+            ...scheduleDetails.Target,
+            EcsParameters: {
+              ...scheduleDetails.Target.EcsParameters,
+              TaskDefinitionArn: newTaskDefinitionArn,
+            },
+          },
+        };
+        const updateOutput = await scheduler.send(
+          new UpdateScheduleCommand(updatedSchedule)
+        );
+
+        if(updateOutput?.ScheduleArn && updateOutput.$metadata.httpStatusCode === 200) {
+            console.log(`Schedule updated successfully for ${scheduleDetails.ScheduleName}`);
+        } else {
+            core.setFailed(`Failed to update schedule for ${scheduleDetails.ScheduleName}. Update response: ${updateOutput}`);
+        }
+      })
+    );
+    console.log('Update complete');
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+})();
